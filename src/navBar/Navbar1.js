0210902e84e5bea9104db839181a893026d76f3b
Steffyvarghese64 <steffy.varghese64@gmail.com>
2023-02-17 13:14:41 -0600

    Navbar copy

--- src/navBar/Navbar1.js
+++ src/navBar/Navbar1.js
@@ -1,16 +1,32 @@
 import "./NavBar.css"
-import React from 'react'
+import React, {useRef} from 'react';
+import {FaTimes, FaBars} from "react-icons/fa"
 
 export const Navbar1 = () => {
+
+  const navRef=useRef()
+
+  const showNavbar=()=>{
+    navRef.current.classList.toggle("rsponsive_nav")
+  }
+
   return (
-    <nav>
+    <nav ref={navRef}>
+     
         <ul className="list">
             <li className="items">SignIn</li>
             <li className="items">SignOut</li>
             <li className="items">Support</li>
         </ul>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes/>
+        </button>
+        <button className="nav-btn" onClick={showNavbar}><FaBars/></button>
     </nav>
+    
   )
 }
+
+
 
 
